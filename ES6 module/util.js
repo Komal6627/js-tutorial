b15b70a7961c9574099df4865ac032e56745fd84
Komal Killedar <komal.killedar@corp.ola.com>
2022-07-11 11:54:59 +0530

    generator is pending

---
+++ ES6 module/util.js
@@ -0,0 +1,16 @@
+//  function double(n) {
+//     return n * 2;
+// }
+
+//  const name = "Komal";
+
+//  export {name, double} ;
+
+
+// export default function sayName() {
+//     console.log("Komal");
+// }
+
+export { water } from "./helper.js"
+
+
